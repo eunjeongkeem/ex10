35bcb4e676df1520e0fc137ad9e866d46cee26b6
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-10-07 10:52:23 +0900

    test

---
+++ Exam10/src/kr/itedu/ex10/test/CalcTest.java
@@ -0,0 +1,17 @@
+package kr.itedu.ex10.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import kr.itedu.ex10.Calc;
+
+class CalcTest {
+
+	@Test
+	void test() {
+	
+	}
+
+}
